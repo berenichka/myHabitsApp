a14cba8247f6ac166e1579c361448f2ecbb0a58f
Veronika <pozhiloy@gmail.com>
2021-08-02 22:39:44 +0600

    1. Counter and buttons color change and updated corrected 2. AlertController and habit deletion created 3. Unnecessary constraints deleted

---
+++ MyHabits/Habits/HabitDetailTableHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  HabitDetailTableHeaderView.swift
+//  MyHabits
+//
+//  Created by Veronika Torushko on 18.07.2021.
+//
+
+import Foundation
---
+++ MyHabits/Habits/HabitDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HabitDetailTableViewCell.swift
+//  MyHabits
+//
+//  Created by Veronika Torushko on 10.07.2021.
+//
+
+import UIKit
+
+class HabitDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
